79135e1c05c2a68ff2388478ddf3c7eb1ff750dd
kanashikia <49899875+kanashikia@users.noreply.github.com>
2024-05-21 17:49:34 +0200

    feat: add first call api (rework needed)

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client";
+import { getLastMovie } from "../services/api/tmdb";
+import { useEffect, useState } from "react";
 
-export default function Browse() {
+export const Browse: React.FC = () => {
+  const [tmdbData, setTmdbData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchTmdbMovie = async () => {
+      try {
+        const data = await getLastMovie();
+        setTmdbData(data);
+      } catch (err) {
+        // setError("Error retrieving movie data");
+      } finally {
+        // setLoading(false);
+      }
+    };
+
+    fetchTmdbMovie();
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      browse
+      {tmdbData?.original_title}
+      <br />
+      {tmdbData?.overview}
     </main>
   );
-}
+};
+
+export default Browse;
