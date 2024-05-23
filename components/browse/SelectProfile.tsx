1c7291dd1f16ea2aa8911a64d2944f437011d65c
Theo GUERRAULT <theo.guerrault@capgemini.com>
2024-05-23 13:36:21 +0200

    feat: delete tailwind css

--- components/browse/SelectProfile.tsx
+++ components/browse/SelectProfile.tsx
@@ -1,31 +1,42 @@
 import { getImages } from '@/lib/getImages';
 import Image from 'next/image';
 
-export default async function SelectProfile() {
-  const images: string[] = await getImages();
+export default function SelectProfile() {
+  const images: string[] = getImages();
   return (
-    <>
-      <h1>Qui est-ce ?</h1>
-      <ul className="choose-profil">
-        {images &&
-          images.map((src: string, index: number) => (
-            <li className="profile" key={`profil ${index}`}>
-              <div>
-                <a className="profile-link">
-                  <div className="relative rounded max-w-[200px] max-h-[200px] min-h-[84px] min-w-[84px] w-[10vw] h-[10vw] overflow-hidden">
-                    <Image
-                      src={src}
-                      alt={`Profil ${index}`}
-                      fill={true}
-                      priority={index === 0} // Priorite pour charger la premiere image, mode lazy par defaut
-                    />
+    <div className="text-center">
+      <div className="flex flex-col">
+        <div className="block maw-w-[80%]">
+          <h1>Qui est-ce ?</h1>
+          <ul className="my-[2vh] flex flex-wrap">
+            {images &&
+              images.map((src: string, index: number) => (
+                <li
+                  className="block mr-[2vw]"
+                  key={`profil ${index}`}
+                >
+                  <div className="">
+                    <a className="w-fit">
+                      <div
+                        className="box-border border border-transparent relative rounded max-w-[200px] max-h-[200px] min-h-[84px] min-w-[84px] w-[10vw] h-[10vw] overflow-hidden
+                      hover:border hover:border-white"
+                      >
+                        <Image
+                          src={src}
+                          alt={`Profil ${index}`}
+                          fill={true}
+                          priority={index === 0} // Priorite pour charger la premiere image, mode lazy par defaut
+                        />
+                      </div>
+                      <span className="profile-name">Name</span>
+                    </a>
                   </div>
-                  <span className="profile-name">Name</span>
-                </a>
-              </div>
-            </li>
-          ))}
-      </ul>
-    </>
+                </li>
+              ))}
+          </ul>
+        </div>
+        <a className="border border-white">Gérer les profils</a>
+      </div>
+    </div>
   );
 }
--- lib/getImages.ts
+++ lib/getImages.ts
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export async function getImages() {
+export function getImages() {
   const imagesDir = path.join(process.cwd(), 'public/images/profil');
   const filenames = fs.readdirSync(imagesDir);
   return filenames.map((name) => `/images/profil/${name}`);
